bd3433ea831e903cc3eee4fff80beeea71f6b740
CharlotteMoureau <76161804+CharlotteMoureau@users.noreply.github.com>
2023-05-14 12:03:52 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
 import { recipesData } from "./data/data.js";
 
-const recipesList = recipesData.recipes.map((recipeList) => {
-  return recipeList;
-});
-
 function getRecipe() {
+  const recipesList = recipesData.recipes.map((recipeList) => {
+    return recipeList;
+  });
+
   const randomRecipe = Math.floor(Math.random() * recipesList.length);
   const recipe = recipesList[randomRecipe];
 
+  const ulIngredients = document.getElementById("recipe-ingredients");
+
   document.getElementById("btn").innerHTML = "Une autre !";
-  document.getElementById("recipePicture").src = `./img/dishes/${recipe.url}`;
-  document.getElementById("recipeTitle").innerHTML = recipe.name;
-  document.getElementById(
-    "recipeTime"
-  ).innerHTML = `Temps de préparation : ${recipe.time}`;
-  document.getElementById(
-    "recipeServings"
-  ).innerHTML = `<i class="fa-solid fa-user"></i> ${recipe.serving}`;
 
-  const ulIngredients = document.getElementById("recipeIngredients");
+  if (recipe.flag == "random") {
+    document.getElementById(
+      "recipe-picture"
+    ).src = `./img/dishes/${recipe.pic}`;
+    document.getElementById("recipe-title").innerHTML = recipe.name;
+    document.getElementById("recipe-time").innerHTML = "";
+    document.getElementById("recipe-servings").innerHTML = "";
+    document.getElementById("cta-link").classList.add("hidden");
 
-  while (ulIngredients.hasChildNodes()) {
-    ulIngredients.removeChild(ulIngredients.firstChild);
+    while (ulIngredients.hasChildNodes()) {
+      ulIngredients.removeChild(ulIngredients.firstChild);
+    }
+  } else {
+    document.getElementById(
+      "recipe-picture"
+    ).src = `./img/dishes/${recipe.pic}`;
+    document.getElementById("recipe-title").innerHTML = recipe.name;
+    document.getElementById(
+      "recipe-time"
+    ).innerHTML = `Temps de préparation : ${recipe.time}`;
+    document.getElementById(
+      "recipe-servings"
+    ).innerHTML = `<i class="fa-solid fa-user"></i> ${recipe.serving}`;
+
+    while (ulIngredients.hasChildNodes()) {
+      ulIngredients.removeChild(ulIngredients.firstChild);
+    }
+
+    recipe.ingredients.forEach((element) => {
+      let ingredient = document.createElement("li");
+
+      ingredient.innerHTML = `<div class="ingredients"><img src="./img/ingredients/${element.pic}.png"/><p>${element.name}</p></div>`;
+      ulIngredients.appendChild(ingredient);
+    });
+    console.log(recipe.link);
+
+    if (recipe.link === undefined) {
+      document.getElementById("cta-link").classList.add("hidden");
+    } else {
+      document.getElementById("cta-link").classList.remove("hidden");
+      document.getElementById("recipe-link").href = recipe.link;
+      document.getElementById("recipe-link").innerHTML = "Voir la recette";
+    }
+
+    //   const ulSteps = document.getElementById("recipeSteps");
+
+    //   while (ulSteps.hasChildNodes()) {
+    //     ulSteps.removeChild(ulSteps.firstChild);
+    //   }
+
+    //   recipe.steps.forEach((element) => {
+    //     let step = document.createElement("li");
+
+    //     step.innerHTML = `<div><h3>${element.step}</h3><p>${element.instruction}</p></div>`;
+    //     ulSteps.appendChild(step);
+    //   });
   }
-
-  recipe.ingredients.forEach((element) => {
-    let ingredient = document.createElement("li");
-
-    ingredient.innerHTML = `<div class="ingredients"><img src="./img/ingredients/${element.pic}"/><p>${element.name}</p></div>`;
-    ulIngredients.appendChild(ingredient);
-  });
-
-  const ulSteps = document.getElementById("recipeSteps");
-
-  while (ulSteps.hasChildNodes()) {
-    ulSteps.removeChild(ulSteps.firstChild);
-  }
-
-  recipe.steps.forEach((element) => {
-    let step = document.createElement("li");
-
-    step.innerHTML = `<div><h3>${element.step}</h3><p>${element.instruction}</p></div>`;
-    ulSteps.appendChild(step);
-  });
 }
 
 document.getElementById("btn").addEventListener("click", getRecipe);
